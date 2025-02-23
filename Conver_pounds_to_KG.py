a6db128bebb8df7b982b79afe660581ec253a5cc
Phindulo.dev <95999814+muthelophindulo@users.noreply.github.com>
2025-02-23 10:36:00 +0200

    Conver_pounds_to_KG.py

--- Conver_pounds_to_KG.py
+++ Conver_pounds_to_KG.py
@@ -1,9 +1,14 @@
 #convert pounds into kilograms 
 # 1pound == 0.454 kilograms
+
 #inputs
 No_pounds = float(input("Enter the amount of pounds to be converted: "))
-#1 pound == 0.454kg
-#200pounds == xkg
+
 #convertion
 kilograms = No_pounds * 0.454
 print(f"{No_pounds} pounds to kilograms is {kilograms} kilograms")
+
+"""
+  no_pounds = 250
+  output = 113.5 kilograms
+"""
