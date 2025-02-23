325b815a073d1c3087e46ce7f10d5d63199c4fb1
Phindulo.dev <95999814+muthelophindulo@users.noreply.github.com>
2025-02-23 10:31:21 +0200

    volume_calculator.py

--- volume.py
+++ volume.py
@@ -4,7 +4,7 @@ radius = float(input("Enter the radius: "))
 length = float(input("Enter the lenth: "))
 pi = 3.14
 
-'''the formulas
+'''
     area = radius^2 * pi
     volume = area * length
     constant pi = 3.14
@@ -13,6 +13,12 @@ pi = 3.14
 area = radius **2 * pi
 volume = area * length
 
+
+
 #print the outcomes
 print(f"The area is: {area},and the volume is {volume}")
-
+'''
+    radius = 5.9
+    length = 12
+    output = "The area is: 109.30340000000001,and the volume is 1311.6408000000001"
+'''
