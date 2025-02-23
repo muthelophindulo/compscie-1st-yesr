c437e18da01e2eb143964d7cab9ac25f23fe98aa
Phindulo.dev <95999814+muthelophindulo@users.noreply.github.com>
2025-02-23 10:17:03 +0200

    velocitycalculator.py

--- velocitycalculator.py
+++ velocitycalculator.py
@@ -1,7 +1,9 @@
-#the formula is v(fianl) = v(initial) + accelaration * time
 #inputs of the initial velocity of the car and its accelaration and time
+
 initial_velocity = float(input("Enter the initial velocity of the car: "))
+
 accelaration = float(input("Enter the accelaration of the car: "))
+
 time = float(input("Enter the time: "))
 
 #add everything up to produce the final velocity
@@ -10,3 +12,8 @@ time = float(input("Enter the time: "))
 final_velocity = initial_velocity + accelaration * time
 
 print(f"The final velocity of the car is {final_velocity}")
+
+#initial_velocity = 15.4
+#accelaration = 9.8
+#time = 2.3
+#output = 
